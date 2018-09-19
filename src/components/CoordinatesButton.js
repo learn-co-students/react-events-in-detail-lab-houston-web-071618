60ebef17df4522b004994ad8ba55e93c2951e4e5
farverio <github email address>
2018-09-19 18:06:55 -0500

    in progress

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,16 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+class CoordinatesButton extends Component {
+  createArray(e) {
+    // debugger
+    this.onReceiveCoordinates([e.clientX, e.clientY])
+  }
+  
+  render() {
+    return (
+      <button onClick={e => this.createArray(e)}></button>
+    )
+  }  
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+class DelayedButton extends Component {
+  createArray(e) {
+  }
+  
+  render() {
+    return (
+      <button onClick={e => setTimeout( this.onDelayedClick(e), this.props.delay)}></button>
+    )
+  }  
+}
+
+export default DelayedButton
